f4da6c9a66e474618b524aedd6e5143b262f7e8c
nsmairat <nilasmairat@gmail.com>
2024-06-12 11:26:22 -0700

    Added basic search engine

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-var searchFormElement = document.querySelector("#search-form");
-console.log(searchFormElement);
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInput.value;
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
