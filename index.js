91355da328ee3c78645c10a4a0dd49e40800b9cf
nsmairat <nilasmairat@gmail.com>
2024-06-23 18:35:22 -0700

    Injected html from js

--- index.js
+++ index.js
@@ -50,7 +50,40 @@ function handleSearchSubmit(event) {
 
   searchCity(searchInput.value);
 }
+//new project forecast starts here...
+function displayForecast() {
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
 
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+<div class="row">
+<div class="col-2">
+  <div class="weather-forecast-date">${day}</div>
+
+ <img
+   src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/rain-night.png"
+  alt=""
+  width="42"
+/>
+<div class="weather-forecast-temperature">
+ <span class="weather-forecast-temperature-max">18</span>
+ <span class="weather-forecast-temperature-min">12</span>
+ </div>
+</div>
+ </div>
+
+`;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
+//new project forecast finishes here...
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 searchCity("Locorotondo");
+displayForecast();
